98e149a5dc4af7b37168acc5f87b5bfdc0956b12
kljohnson2 <kylerljohnson@icloud.com>
2017-10-29 22:37:10 -0400

    Add tests for get /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: "First test todo"
+},
+{
+    text: "Second test todo"
+}]
+
 // testing lifecycle method
 beforeEach(done => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +33,7 @@ describe('POST /todos', () => {
                 if(err)
                     return done(err);
                 
-                Todo.find().then(todos => {
+                Todo.find({text}).then(todos => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -44,9 +53,24 @@ describe('POST /todos', () => {
                     return done(err);
 
                 Todo.find().then(todos => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(err => done(err));
+            });
+    });
+});
+
+describe('GET /todos', () => {
+
+    it('Should get all todos', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2);
             })
-    })
-})
+            .end(done);
+    });
+
+
+});
