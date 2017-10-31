93bd5d9dbddd634cf263e2e9724e6fc1365d41bb
kljohnson2 <kylerljohnson@icloud.com>
2017-10-31 09:21:19 -0400

    Add start script, specify engines, and create port variable for heroku deployment

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -45,6 +46,6 @@ app.get('/todos/:id', (req, res) => {
 
 module.exports = {app};
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 })
