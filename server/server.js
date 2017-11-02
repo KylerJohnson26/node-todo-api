899256b8a757d1f02c8faf63daecbe7512fba214
kljohnson2 <kylerljohnson@icloud.com>
2017-11-02 08:42:15 -0400

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,6 +44,19 @@ app.get('/todos/:id', (req, res) => {
     }).catch(err => res.status(400).send(err));
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then(todo => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(err => res.status(400).send(err));
+})
+
 module.exports = {app};
 
 app.listen(port, () => {
