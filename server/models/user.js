1fa73109814da2525141b4ec90e91c17aab78e33
Kyler Johnson <kylerjohnson@Kylers-MacBook-Pro.local>
2017-11-23 23:21:06 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,17 @@ UserSchema.methods.generateAuthToken = function() {
     })
 }
 
+UserSchema.methods.removeToken = function(token) {
+    let user = this;
+
+    return user.update({
+        // removes entire object in array with matching token
+        $pull: {
+            tokens: {token}
+        }
+    })
+}
+
 UserSchema.statics.findByToken = function(token) {
     let User = this;
     let decoded;
